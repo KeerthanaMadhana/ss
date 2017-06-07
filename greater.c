e91b86c4143fda28135ef5bac42ff17aaa792dce
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-06-07 14:47:45 +0530

    Add files via upload

---
+++ greater.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+    
+int a,b,c; 
+ printf("enter the number");
+ 
+   scanf("%d,%d,%d",&a,&b,&c);
+   
+ if(a>b && a>c)
+    
+printf("a is greater");
+ 
+   else if(b>a && b>c)
+  
+  printf("b is greater");
+ 
+   else if(c>a && c>b)
+ 
+   printf("c is greater");
+ 
+   return 0;
+
+}
