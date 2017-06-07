f198ade6f1cea78e790816c2524e0ab1931fb4fe
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-06-07 14:25:03 +0530

    Add files via upload

---
+++ alphaornot.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    char c,n;
+
+    printf("enter the character");
+
+    scanf("%c",&c);
+
+    if(c>='A' &&c<='Z' || c>='a'&&c<='z')
+
+    printf("the entered character is alphabet");
+
+    else
+
+    printf("the entered character is not alphabet");
+
+    return 0;
+
+}
